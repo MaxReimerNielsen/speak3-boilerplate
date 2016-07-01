1dc8ac9937485f54cc37a4cc839d30f7e8d9c395
MaxReimerNielsen <max.reimer.nielsen@gmail.com>
2016-07-01 10:40:52 +0200

    Added temporary BooksContianer Actions test.

---
+++ src/containers/Books/BooksContainer_actions.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { HOME_GETDATA } from './ActionTypes';
+import * as actions from './BooksContainer_actions';
+
+describe('ActionTypes', () => {
+  beforeEach(() => {
+    /*
+      TODO: Please re-investigate this.
+      Ideas to consider:
+        - Introducing Redux-Saga
+        - Introducing polyfills for fetch and promise in Node and Internet Explorer
+    */
+    global.fetch = () => ({ then: () => 'HelloWorld' });
+  });
+
+  afterEach(() => {
+    delete global.fetch;
+  });
+
+  it('should have a getData method', () => {
+    expect(actions.getData).not.to.be.equal(undefined);
+  });
+
+  it('calling getData should return ActionType and given payload', () => {
+    expect(actions.getData()).to.deep.equal({
+      type: HOME_GETDATA,
+      payload: 'HelloWorld',
+    });
+  });
+});
