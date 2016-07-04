4f132847cf89140d44ac2b83351e413fbfd9331e
MaxReimerNielsen <max.reimer.nielsen@gmail.com>
2016-07-04 08:39:29 +0200

    Fixed validation errors

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -1,10 +1,8 @@
 const webpackBasicConfig = require('./webpack-basic.config');
 
 const config = Object.assign({}, webpackBasicConfig, {
-  devtool: null,
-  preLoaders: [],
   externals: {
-    'cheerio': 'window',
+    cheerio: 'window',
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': 'window',
     'react/addons': 'window',
